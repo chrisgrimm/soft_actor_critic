d4828b8ec06e1940bbfb91f4676a0f2df4df2b78
Ethan Brooks <ethanabrooks@gmail.com>
2018-03-18 22:15:57 -0400

    added argparse

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 import gym
 from chaser import ChaserEnv
@@ -7,6 +9,7 @@ from replay_buffer.replay_buffer import ReplayBuffer2
 from networks.policy_mixins import MLPPolicy, GaussianPolicy, CategoricalPolicy
 from networks.value_function_mixins import MLPValueFunc
 from networks.network_interface import AbstractSoftActorCritic
+
 
 def build_agent(env):
     state_shape = env.observation_space.shape
@@ -22,26 +25,27 @@ def build_agent(env):
     class Agent(PolicyType, MLPPolicy, MLPValueFunc, AbstractSoftActorCritic):
         def __init__(self, s_shape, a_shape):
             super(Agent, self).__init__(s_shape, a_shape)
+
     return Agent(state_shape, action_shape)
 
+
 def build_action_converter(env):
-    if type(env.action_space) is spaces.Discrete:
-        def converter(a):
-            return np.argmax(a)
-    else:
-        def converter(a):
+    def converter(a):
+        if type(env.action_space) is spaces.Discrete:
+                return np.argmax(a)
+        else:
             a = np.tanh(a)
             h, l = env.action_space.high, env.action_space.low
             return ((a + 1) / 2) * (h - l) + l
     return converter
 
 
+def run_training(env, buffer_size, reward_scale, batch_size, num_train_steps):
+    if env == 'chaser':
+        env = ChaserEnv()
+    else:
+        env = gym.make(env)
 
-def run_training(env):
-    buffer_size = 10**7
-    num_train_steps = 1
-    batch_size = 32
-    reward_scale = 1./10.
     s1 = env.reset()
 
     agent = build_agent(env)
@@ -54,37 +58,43 @@ def run_training(env):
     while True:
         a = agent.sample_actions([s1])
         a = a[0]
-        #s2, r, t, info = env.step(2*a)
-        #s2, r, t, info = env.step(np.argmax(a))
+        # s2, r, t, info = env.step(2*a)
+        # s2, r, t, info = env.step(np.argmax(a))
         s2, r, t, info = env.step(action_converter(a))
         time_steps += 1
 
         episode_reward += r
-        #env.render()
+        # env.render()
         r /= reward_scale
-        #print(s1)
+        # print(s1)
         buffer.append(s1, a, r, s2, t)
         if len(buffer) >= batch_size:
             for i in range(num_train_steps):
                 s1_sample, a_sample, r_sample, s2_sample, t_sample = buffer.sample(batch_size)
                 [v_loss, q_loss, pi_loss] = agent.train_step(s1_sample, a_sample, r_sample, s2_sample, t_sample)
-                #print('v_loss', v_loss, 'q_loss', q_loss, 'pi_loss', pi_loss)
+                # print('v_loss', v_loss, 'q_loss', q_loss, 'pi_loss', pi_loss)
         s1 = s2
         if t:
             s1 = env.reset()
-            #summary = tf.Summary()
-            #summary.value.add(tag='episode reward', simple_value=episode_reward)
-            #agent.tb_writer.add_summary(summary, episodes)
-            #agent.tb_writer.flush()
+            # summary = tf.Summary()
+            # summary.value.add(tag='episode reward', simple_value=episode_reward)
+            # agent.tb_writer.add_summary(summary, episodes)
+            # agent.tb_writer.flush()
             print('Episode %s\t Time Steps: %s\t Reward: %s' % (episodes, time_steps, episode_reward))
             episode_reward = 0
             episodes += 1
 
 
-env = gym.make('HalfCheetah-v2')
-#env = gym.make('CartPole-v0')
-#env = gym.make('Pendulum-v0')
-#env = ChaserEnv()
-#env = gym.make('MountainCarContinuous-v0')
-run_training(env)
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', default='HalfCheetah-v2')
+    parser.add_argument('--buffer-size', default=int(10 ** 7), type=int)
+    parser.add_argument('--num-train-steps', default=1, type=int)
+    parser.add_argument('--batch-size', default=32, type=int)
+    parser.add_argument('--reward-scale', default=1/10., type=float)
+    args = parser.parse_args()
+    run_training(env=args.env,
+                 buffer_size=args.buffer_size,
+                 reward_scale=args.reward_scale,
+                 batch_size=args.batch_size,
+                 num_train_steps=args.num_train.steps)
