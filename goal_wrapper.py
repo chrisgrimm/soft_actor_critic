7b320b8cc4156962d93f4561c7b0a5a02b382573
Ethan Brooks <ethanabrooks@gmail.com>
2018-04-26 18:09:39 -0400

    pulled in chriss goal_wrapper

--- goal_wrapper.py
+++ goal_wrapper.py
@@ -43,15 +43,21 @@ class GoalWrapper:
 
     @abstractmethod
     def final_goal(self):
-        pass 
+        pass
+
     def step(self, action):
         s2, r, t, info = self.env.step(action)
         new_s2 = self.obs_from_obs_part_and_goal(s2, self.final_goal())
         new_r = self.reward(s2, self.final_goal())
-        self.current_trajectory.append((self.current_state, action, new_r, new_s2, t))
+        if r > 0 and t and new_r != 100:
+            print(s2, r, t)
+            input('waiting...')
+        #    raise Exception('This shouldnt be able to happen')
+        new_t = self.terminal(s2, self.final_goal()) or t
+        self.current_trajectory.append((self.current_state, action, new_r, new_s2, new_t))
 
         self.current_state = new_s2
-        return new_s2, new_r, t, {'base_reward': r}
+        return new_s2, new_r, new_t, {'base_reward': r}
 
     def reset(self):
         self.feed_new_trajectory_to_buffer(self.current_trajectory)
@@ -78,29 +84,30 @@ class GoalWrapper:
             new_r = self.reward(sp_obs_part, final_goal)
             new_t = self.terminal(sp_obs_part, final_goal) or t
             yield new_s, a, new_r, new_sp, new_t
-            if new_t:
+            if new_t == True:
                 break
 
     def feed_new_trajectory_to_buffer(self, trajectory):
         for (s, a, r, sp, t) in self.recompute_trajectory(trajectory):
-            self.buffer.append(s, a, r / self.reward_scaling, sp, t)
+            self.buffer.append(s, a, r / self.reward_scaling, sp ,t)
 
 
 class MountaincarGoalWrapper(GoalWrapper):
     '''
     new obs is [pos, vel, goal_pos]
     '''
-
     def obs_part_to_goal(self, obs_part):
         return np.array([obs_part[0]])
 
     def reward(self, obs_part, goal):
-        dist = np.abs(obs_part[0] - goal[0])
-        return 100 if dist < 0.03 else 0
+        return 100 if obs_part[0] >= goal[0] else 0
+        #dist = np.abs(obs_part[0] - goal[0])
+        #return 100 if dist < 0.03 else 0
 
     def terminal(self, obs_part, goal):
-        dist = np.abs(obs_part[0] - goal[0])
-        return (dist < 0.03)
+        #dist = np.abs(obs_part[0] - goal[0])
+        return (obs_part[0] >= goal[0])
+        #return (dist < 0.03)
 
     def get_obs_part(self, obs):
         return obs[:2]
