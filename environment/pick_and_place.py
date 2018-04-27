0ad54014ef456db9fecdcdadace9a70335c8364c
Ethan Brooks <ethanabrooks@gmail.com>
2018-04-27 12:37:35 -0400

    back to reward 1 but this time, no termination

--- environment/pick_and_place.py
+++ environment/pick_and_place.py
@@ -131,12 +131,13 @@ class PickAndPlaceEnv(MujocoEnv):
         return gripper_at_goal and block_at_goal
 
     def _compute_terminal(self, goal, obs):
-        return self._achieved_goal(goal, obs)
+        return False
+        # return self._achieved_goal(goal, obs)
 
     def _compute_reward(self, goal, obs):
         if self._achieved_goal(goal, obs):
             print('Achieved goal')
-            return 100
+            return 1
         elif self._neg_reward:
             return -.0001
         else:
