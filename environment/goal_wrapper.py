92b678ba025313fa0cb5f0bbabb2b4b32fa99edc
Ethan Brooks <ethanabrooks@gmail.com>
2018-05-05 08:45:15 -0400

    only terminate at max_steps

--- environment/goal_wrapper.py
+++ environment/goal_wrapper.py
@@ -98,14 +98,14 @@ class PickAndPlaceGoalWrapper(GoalWrapper):
     def final_goal(self):
         return self.env.goal()
 
-    def step(self, action):
-        s2, r, t, info = self.env.step(action)
-        new_r = self.reward(s2, self.final_goal())
-        if self.terminal(s2, self.final_goal()):
-            new_s2 = self.reset()
-        else:
-            new_s2 = State(obs=s2, goal=self.final_goal())
-        return new_s2, new_r, self.env.hit_max_steps(), {'base_reward': r}
+    # def step(self, action):
+    #     s2, r, t, info = self.env.step(action)
+    #     new_r = self.reward(s2, self.final_goal())
+    #     if self.terminal(s2, self.final_goal()):
+    #         new_s2 = self.reset()
+    #     else:
+    #         new_s2 = State(obs=s2, goal=self.final_goal())
+    #     return new_s2, new_r, self.env.hit_max_steps(), {'base_reward': r}
 
     @staticmethod
     def obs_from_obs_part_and_goal(state):
