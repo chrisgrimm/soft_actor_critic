07f2ff3b0ac8ab49eab8bb1277955f3ef15ac7fa
Ethan Brooks <ethanabrooks@gmail.com>
2018-05-06 12:06:31 -0400

    init

--- environment/pick_and_place.py
+++ environment/pick_and_place.py
@@ -73,10 +73,10 @@ class PickAndPlaceEnv(MujocoEnv):
         # self._current_orienation = None
 
     def reset_qpos(self):
-        # block_joint = self.sim.jnt_qposadr('block1joint')
-        #
-        # self.init_qpos[block_joint + 3] = np.random.uniform(0, 1)
-        # self.init_qpos[block_joint + 6] = np.random.uniform(-1, 1)
+        block_joint = self.sim.jnt_qposadr('block1joint')
+
+        self.init_qpos[block_joint + 3] = np.random.uniform(0, 1)
+        self.init_qpos[block_joint + 6] = np.random.uniform(-1, 1)
 
 
         # self.init_qpos = np.array([4.886e-05,
