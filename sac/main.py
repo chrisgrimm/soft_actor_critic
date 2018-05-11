552f1e816c6f5a9709c6f7c91ac561674f128479
Ethan Brooks <ethanabrooks@gmail.com>
2018-05-10 23:43:48 -0400

    fixed the action-converter bug

--- sac/main.py
+++ sac/main.py
@@ -51,7 +51,7 @@ def inject_mimic_experiences(mimic_file, buffer, N=1):
 
 class Trainer:
     def step(self, action):
-        return self.env.step(action)
+        return self.env.step(self.action_converter(action))
 
     def reset(self):
         return self.env.reset()
@@ -106,7 +106,7 @@ class Trainer:
                 [self.state_converter(s1)], sample=(not is_eval_period))
             if render:
                 env.render()
-            s2, r, t, info = self.step(self.action_converter(a))
+            s2, r, t, info = self.step(a)
             if t:
                 print('reward:', r)
 
