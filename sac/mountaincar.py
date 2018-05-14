5c250483fc2c6f8ed2d5406b9911d1a874e9e328
Ethan Brooks <ethanabrooks@gmail.com>
2018-05-14 13:40:52 -0400

    added reward-scale arg to mountaincar

--- sac/mountaincar.py
+++ sac/mountaincar.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', default='HalfCheetah-v2')
     parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--reward-scale', default=1000, type=int)
     parser.add_argument('--logdir', default=None, type=str)
     parser.add_argument('--render', action='store_true')
     parser.add_argument('--save-path', default=None, type=str)
@@ -25,7 +26,7 @@ if __name__ == '__main__':
         n_layers=3,
         layer_size=256,
         learning_rate=3e-4,
-        reward_scale=1e3,
+        reward_scale=args.reward_scale,
         batch_size=32,
         num_train_steps=1,
         logdir=args.logdir,
