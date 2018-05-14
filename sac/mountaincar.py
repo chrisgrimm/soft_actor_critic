a18a2c7d0513e231bcfb255a9e11339e07ea7f05
Ethan Brooks <ethanabrooks@gmail.com>
2018-05-14 13:44:18 -0400

    added activation command line

--- sac/mountaincar.py
+++ sac/mountaincar.py
@@ -4,13 +4,14 @@ import gym
 import tensorflow as tf
 
 from environment.hindsight_wrapper import MountaincarHindsightWrapper
-from sac.train import HindsightPropagationTrainer, HindsightTrainer
+from sac.train import HindsightPropagationTrainer, HindsightTrainer, activation_type
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', default='HalfCheetah-v2')
     parser.add_argument('--seed', default=0, type=int)
     parser.add_argument('--reward-scale', default=1000, type=int)
+    parser.add_argument('--activation', default=tf.nn.relu, type=activation_type)
     parser.add_argument('--logdir', default=None, type=str)
     parser.add_argument('--render', action='store_true')
     parser.add_argument('--save-path', default=None, type=str)
@@ -22,7 +23,7 @@ if __name__ == '__main__':
         max_steps=999,
         seed=args.seed,
         buffer_size=10**7,
-        activation=tf.nn.relu,
+        activation=args.activation,
         n_layers=3,
         layer_size=256,
         learning_rate=3e-4,
